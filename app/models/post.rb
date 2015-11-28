21f625e36d592ed3b862738900c28444b8f01a31
Hamza Sheikh <hsheikhm@gmail.com>
2015-11-28 22:38:39 +0000

    completed feature test: images posted are displayed on home page

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :image, presence: true
+  validates :title, presence: true, length: { minimum: 3 }
+
+end
--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -9,4 +9,14 @@ feature 'posts' do
       expect(page).to have_link 'Make a Post'
     end
   end
+
+  context 'images have been posted' do
+    scenario 'display posts' do
+      image = fixture_file_upload('files/test.jpg', 'image/jpg')
+      Post.create(image: image, title: 'test upload')
+      visit '/posts'
+      expect(page).to have_xpath("//img[contains(@src, 'test.jpg')]")
+    end
+  end
+
 end
