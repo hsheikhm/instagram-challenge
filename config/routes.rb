c1a931e3d1262d888bd748f9330e6302b3ee5e9b
Hamza Sheikh <hsheikhm@gmail.com>
2015-11-29 18:17:29 +0000

    created new controller for likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   get 'posts' => 'posts#index'
 
-  resources :posts do
-    resources :comments
+  resources :posts, shallow: true do
+    resources :comments do
+      resources :likes
+    end
   end
 
 end
