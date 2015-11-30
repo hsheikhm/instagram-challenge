71655c2bdf3170368ed0c3482d66f1e7ae4677cc
Hamza Sheikh <hsheikhm@gmail.com>
2015-11-30 12:36:40 +0000

    completed unit test for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -4,4 +4,12 @@ describe Like, type: :model do
 
   it { is_expected.to belong_to :post }
 
+  context "the likes count starts at 0" do
+
+    it "when a user submits a post" do
+      image = fixture_file_upload("files/test.jpg", "image/jpg")
+      post = Post.create(image: image, title: "test upload")
+      expect(post.likes.count).to eq 0
+    end
+  end
 end
