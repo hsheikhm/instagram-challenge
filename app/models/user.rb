7a745d00a11cd145282c03c039836d9c7f98d253
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-17 23:38:32 +0000

    now trying to add profile image to a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
 
   has_many :posts, dependent: :destroy
+  validates :username, presence: true
+
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :image, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
