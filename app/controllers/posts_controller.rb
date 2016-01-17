54a191715ef08b317a11bf7f6b353ad13f8ef9b7
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-17 23:10:51 +0000

    image displayed now shows user who added post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     @post.save ? (redirect_to posts_path) : (render 'new')
   end
 
