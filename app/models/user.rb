c8f46618f8e7aad68532f84342de49938cbdaa5a
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-17 22:46:22 +0000

    added username to devise user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  # attr_accessible :email, :username, :password, :password_confirmation, :remember_me
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
