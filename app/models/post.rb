439933697b568ef3a778f93feafee9254b02c831
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-18 22:44:08 +0000

    fixed formatting issue for header and footer

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "600x550>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment :image, presence: true
   validates :title, presence: true
