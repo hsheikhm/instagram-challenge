228c45848e8e3efe3a6dc2e48e60f4155eaa6cdb
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-21 10:52:28 +0000

    installed chromehelper for likes test - test now passes

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'liking posts' do
 
   scenario "a user can like a post, which increments the likes count", js: true do
+    sign_up
     post_image
     like_post
     expect(page).to have_content "1 like"
