33ef6ad1b505bb0d5894ee2fb84f72bf3b181ff4
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-21 13:45:47 +0000

    trying to fix root

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   get 'posts' => 'posts#index'
 
-  devise_for :users
+  # devise_for :users
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
 
   resources :posts do
     resources :comments, :likes
